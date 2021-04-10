dfc12ac407eac00cfbfd601e0c0a362268def82e
AlexNexton <cianoc13@gmail.com>
2021-04-10 09:19:24 +0000

    set up enviorment variables and automatic deplyments fix

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import dj_database_url
 
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('iaymq5$wpzm&7$j&v4@4#92y#_23&9*u*&tsq!*_p)-xw24&t_')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'iaymq5$wpzm&7$j&v4@4#92y#_23&9*u*&tsq!*_p)-xw24&t_')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL '))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
